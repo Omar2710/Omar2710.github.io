4719129e366c152536e155cbab354124176eb149
Omar2710 <omar2011101@miuegypt.edu.eg>
2022-12-02 00:04:27 +0200

    changes

---
+++ src/app/Omar/Omar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Omar',
+  templateUrl: './Omar.component.html',
+  styleUrls: ['./Omar.component.css']
+})
+export class OmarComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { OmarComponent } from './Omar/Omar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OmarComponent
   ],
   imports: [
     BrowserModule,
